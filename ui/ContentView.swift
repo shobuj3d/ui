207ca8a077e8c93aa407519d98382b939208249a
shobuj3d <shobuj3d@gmail.com>
2024-02-06 19:06:43 +0600

    Initial Commit

---
+++ ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ui
+//
+//  Created by Mazedur Rahman Shobuj on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ui/uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiApp.swift
+//  ui
+//
+//  Created by Mazedur Rahman Shobuj on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
